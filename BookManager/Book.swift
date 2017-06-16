d367d95b36f38a5b2dbba554edd011c1ef8814f2
Ian8829 <ghjkghj@naver.com>
2017-06-17 05:34:04 +0900

    Initial State

---
+++ BookManager/Book.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Book {
+    var name = ""
+    var genre = ""
+    var author = ""
+
+    func bookPrint() {
+        print("Name : \(name)")
+        print("Genre : \(genre)")
+        print("Author : \(author)")
+    }
+}
---
+++ BookManager/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+var book1 = Book()
+book1.name = "햄릿"
+book1.genre = "비극"
+book1.author = "셰익스피어"
+
+book1.bookPrint()
