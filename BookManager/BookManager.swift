acb9fb93a15e69c9268069a008f3cdbc11f2ce52
Ian8829 <ghjkghj@naver.com>
2017-06-17 06:02:51 +0900

    Manager2

---
+++ BookManager/BookManager.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class BookManager {
+    var bookList = [Book]()
+    // make empty mutable array
+    
+    func addBook(bookObject: Book) {
+        bookList += [bookObject]
+    }
+    
+//    func showAllBook() -> String{
+//    }
+//    
+//    func countBook() -> Int {
+//        
+//    }
+    
+    func findBook() {
+        
+    }
+    
+    func removeBook() {
+        
+    }
+}
--- BookManager/main.swift
+++ BookManager/main.swift
@@ -5,4 +5,26 @@ book1.name = "햄릿"
 book1.genre = "비극"
 book1.author = "셰익스피어"
 
-book1.bookPrint()
+//book1.bookPrint()
+
+var book2 = Book()
+book2.name = "종"
+book2.genre = "전쟁"
+book2.author = "해밍웨이"
+
+//book2.bookPrint()
+
+var book3 = Book()
+book3.name = "죄"
+book3.genre = "사실주의"
+book3.author = "톨스토이"
+
+//book3.bookPrint()
+
+var myBook = BookManager()
+
+myBook.addBook(bookObject: book1)
+myBook.addBook(bookObject: book2)
+myBook.addBook(bookObject: book3)
+
+
