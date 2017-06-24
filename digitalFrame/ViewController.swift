b3c6902fd97ac8906693a6859615d6bed0b8e203
Ian8829 <ghjkghj@naver.com>
2017-06-25 02:17:20 +0900

    Digital Frame3

--- digitalFrame/ViewController.swift
+++ digitalFrame/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imgView:UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let cuteImages = [
+            UIImage(named:"1.jpg")!,
+            UIImage(named:"2.jpg")!,
+            UIImage(named:"3.jpg")!,
+            UIImage(named:"4.jpg")!,
+            UIImage(named:"5.jpg")!,
+            UIImage(named:"6.jpg")!,
+            UIImage(named:"7.jpg")!,
+            UIImage(named:"8.jpg")!,
+            UIImage(named:"9.jpg")!,
+            UIImage(named:"10.jpg")!,
+            ]
+        
+        imgView.animationImages = cuteImages
     }
 
     override func didReceiveMemoryWarning() {
